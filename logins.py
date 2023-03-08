f191ff9a564ebc5aae9e57b48f9dcbb9538d237c
Corius77 <corius.business@gmail.com>
2023-03-08 20:44:04 +0100

    I started at the beginning (Currently program entering the selected order from W24)

--- logins.py
+++
@@ -1,9 +0,0 @@
-#Warsztat24 login details:
-
-w24_company = "COMPANY_NAME"
-w24_name = "NAME"
-w24_password = "PASSWORD"
-
-#ProfiAuto login details:
-profi_auto_id = "ID"
-profi_password = "PASSWORD"
