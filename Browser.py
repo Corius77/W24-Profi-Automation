8c071c9324ec9a03ced3982a3cad81494d17bacc
Corius77 <corius.business@gmail.com>
2023-03-08 14:35:45 +0100

    Changing __int__ to __init__ (Stil not working)

--- Browser.py
+++ Browser.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 class Browser:
-    def __int__(self):
-        self.options = Options()                                    #Settings of the Browser
+    def __init__(self, url)                                         #Settings of the Browser.
+        self.options = Options()
         self.options.add_argument("--window-size=1936,1056")
         self.options.add_argument("--headless")
         self.browser = webdriver.Chrome(options=self.options)
+        self.url = url
 
-    def Open(self, url):                                            #Method to open url
-        self.browser.get(url)
+    def Open(self):                                                 #Method to open url.
+        self.browser.get(self.url)
 
-test = Browser()
-google = "http://google.com"                                        #Testing opening browser with google url
-test.Open(google)
+google = "http://google.com"
+
+test = Browser(google)                                              #Testing opening browser with google url
+test.Open()
 
